22f61d938dcd2345a6e3c8d97ab0395ca3fbee7e
sohamdhamnaskar <sohamdham123@gmail.com>
2021-07-07 20:42:48 +0530

    "[soham]Issue Resolved."

--- src/com/webapp/mypackage/ChromeWeb.java
+++ src/com/webapp/mypackage/ChromeWeb.java
@@ -1,6 +1,7 @@
 package com.webapp.mypackage;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,23 +12,19 @@ public class ChromeWeb {
         System.out.println("Hello World");
         System.setProperty("webdriver.chrome.driver","C:\\Users\\soham\\selenium jars and drivers\\drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         driver.get("https://www.wsupercars.com/website-search/");
         driver.manage().window().maximize();
-
         WebElement CarName = driver.findElement(By.id("s"));
         CarName.sendKeys("Porsche");
-
         WebElement SearchBttn = driver.findElement(By.xpath("/html/body/div/main/section/div[2]/form/button"));
         SearchBttn.click();
-
-
-        WebElement Image = driver.findElement(By.xpath("//a[contains(text(),'2013 PORSCHE 911 GT3 Cup')]"));
-        /*Actions act = new Actions(driver);
-        act.moveToElement(Image).click().build().perform();*/
-
-        Image.click();
+        js.executeScript("window.scrollBy(0,700)");
+        Thread.sleep(3000);
+        //driver.findElement(By.cssSelector("div.wrapper:nth-child(1) main:nth-child(2) section.content div.search-box-holder:nth-child(5) > a.home-box:nth-child(5)")).click();
+        driver.findElement(By.cssSelector("div.wrapper:nth-child(1) section.content div.search-box-holder:nth-child(5) a.home-box:nth-child(5) div.thumb:nth-child(1) > img.attachment-post-thumbnail.size-post-thumbnail.wp-post-image")).click();
+        //driver.findElement(By.xpath("/html[1]/body[1]/div[1]/main[1]/section[1]/div[4]/a[2]/div[1]/img[1]")).click();
+        //Thread.sleep(3000);
         System.out.println("Done");
-
-
     }
 }
